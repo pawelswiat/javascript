06cc142e965f3b99e9b73758a1a6b00fe7a68da2
pawelswiat <pawelswiat89@gmail.com>
2021-04-10 11:43:49 +0200

    1 file modified

--- zadania/podstawy.js
+++ zadania/podstawy.js
@@ -7,14 +7,8 @@
 // const arr3 = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48]
 // const arr4 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51]
 
-// const foo = (passedArray) => {
-// 	let output = [];
-//   for (const [index, arrayValue] of passedArray.entries()) {
-//     if (index > 0 && index % 2 == 0) {
-//       output[output.length] = arrayValue;
-//     }
-//   }
-//   return output;
+// const printEverySecondValue = (passedArray) => {
+//  passedArray.filter((value,index) => index % 2));
 // } 
 
 // console.log(foo(arr3));
