7693b6cf2303fb6fe7140706cfe76e0c09ef9fdb
pawelswiat <pawelswiat89@gmail.com>
2021-04-10 19:59:38 +0200

    1 file modified

--- zadania/podstawy.js
+++ zadania/podstawy.js
@@ -18,17 +18,13 @@
 // Napisz program, który wypisze największą liczbę z tablicy. Tablicę zainicjalizuj przykładowymi wartościami.
 
 
-// const getHighestNumber = (array) => {
-// 	let highestNumber = null;
-//   for (value of array) {
-//   	if (value > highestNumber) {
-//     	highestNumber = value;
-//     }
-//   }
-//   return highestNumber;
-// }
-
-// console.log(getHighestNumber(arr3));
+//  const findHighestValueInArray = (passedArray) => {
+//  	 return passedArray.reduce((accumulator, nextValue) => 
+//    accumulator > nextValue ? accumulator : nextValue)
+//  }
+ 
+ 
+//  console.log(findHighestValueInArray(arr2));
 
 // Słowa z tablicy wielkimi literami¶
 // Napisz program, w którym zdefiniujesz tablicę wartości typu String i zainicjalizujesz ją przykładowymi wartościami. Skorzystaj z pętli for-each, aby wypisać wszystkie wartości tablicy ze wszystkimi literami zamienionymi na wielkie. Skorzystaj z funkcjonalności toUpperCase wartości typu String, którą poznaliśmy już w jednym z poprzednich rozdziałów.
@@ -36,12 +32,8 @@
 // const arr = ["banana", "pinapple", "grape", "orange", "apple", "grape"];
 
  
-// const convertToUpperCase = (passedArray) => {
-//     let output = [];
-//   for (item of passedArray) {
-//    output[output.length] = item.toUpperCase();
-//   }
-//   return output;
+// const convertToUpperCase = (passedArray) => { 
+// 	return passedArray.map(val => val.toUpperCase());
 // }
 
 // console.log(convertToUpperCase(arr));
@@ -52,21 +44,11 @@
 // const arr = ["banana", "pinapple", "grape", "orange", "apple", "grape"];
 
 
-// const reverseWordsAndSwapLetters = (passedArray) => {
-// 	let output = [];
-//   for (let i = passedArray.length - 1; i >= 0; i--) {
-//   let swappedLettersWord = "";
-//   	for (let j = passedArray[i].length - 1; j >= 0; j--) {
-//     	swappedLettersWord += passedArray[i][j];
-//       if (j == 0) {
-//       	output[output.length] = swappedLettersWord;
-//     	}
-//     }
-//   }
-//   return output;
-// } 
+//  const reverseWordsAndSwapLetters = (passedArray) => {
+//    return passedArray.reverse().map((val) => Array.from(val).reverse().join(""))
+//  }
 
-// console.log(reverseWordsAndSwapLetters(arr));
+//  console.log(reverseWordsAndSwapLetters(arr));
 
 
 // Napisz program, który pobierze od użytkownika osiem liczb, zapisze je w tablicy, a następnie posortuje tą tablicę rosnąco i wypisze wynik sortowania na ekran. Dla przykładu, dla liczb 10, -2, 1, 100, 20, -15, 0, 10, program wypisze -15, -2, 0, 1, 10, 10, 20, 100. Zastanów się, jak można posortować ciąg liczb i spróbuj zaimplementować swoje rozwiązanie. Przetestuj je na różnych zestawach danych. Możesz też skorzystać z jednego z popularnych algorytmów sortowania, np. sortowania przez wstawianie. Opis tego algorytmu znajdziesz w internecie.
@@ -74,22 +56,10 @@
 
 // const arr = [10, -3, 7, 100, 22, 21, 453];
 
-// const sortFunction = (passedArray) => {
-// 	let output = passedArray;
-//   let isSorted = false;
-//    while (!isSorted) {
-//      isSorted = true;
-//      for (const[index, value] of output.entries()) {
-//         if (output[index] > output[index + 1]) {
-//           isSorted = false;
-//           let storeVal = output[index];
-//          output[index] = output[index + 1];
-//          output[index + 1] = storeVal;
-//         }
-//      }
-//   }
-// 	return output;
+// const sortFunction = (passedArray) => { 
+// 	return passedArray.sort((a, b) => a - b);
 // }
+
 // console.log(sortFunction(arr2));
 
 
@@ -102,15 +72,16 @@
 // const arr2 = ["Anglia", "Ukraina", "Czechy"];
 
 // const compareArray = (arrayOne, arrayTwo) => {
-//   if (arrayOne.length !== arrayTwo.length) {
-//     return false;
+//     return arrayOne.every(value => 
+//        arrayTwo.includes(value) && (arrayOne.indexOf(value) == arrayTwo.indexOf(value))); 
 //   }
-//   for (const [index, value] of arr1.entries()) {
-//     if (arrayOne[index] !== arrayTwo[index]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
 
-// console.log(compareArray(arr1, arr2))
+// console.log(compareArray(arr1, arr2));
+
+
+// const compareArray = (arrayOne, arrayTwo) => {
+//     return arrayOne.every(value => 
+//        arrayOne.indexOf(value) == arrayTwo.indexOf(value)); 
+//   }
+
+// console.log(compareArray(arr1, arr2)); 
